f56c6123b932a3c1f0138db107bdce987d48a26d
mlazzell17@gmail.com <MicheSi>
2020-06-03 15:28:07 -0700

    completed no_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,21 @@
 def no_dups(s):
-    # Your code here
+    # create dictonary for duplicate words
+    duplicates = {}
+    # create list to store string w/o duplicates
+    no_duplicates = []
+    # split string at spaces into list
+    words = s.split(' ')
+    # iterate through list
+    for word in words:
+        # if word not already in dictionary
+        if word not in duplicates:
+            duplicates[word] = 1
+            # add word to list
+            no_duplicates.append(word)
+    # join words into a string with space in between them
+    return ' '.join(no_duplicates)
+
+
 
 
 
